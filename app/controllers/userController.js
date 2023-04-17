d3ecdd2a530782dcb35bd671a27bffc53f192846
EchterhoffAlexandre <echterhoff.a@gmail.com>
2023-04-17 11:38:01 +0200

    userController + CRUD

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -1,9 +1,22 @@
 const { User, Shop, Transaction, Family, Collection, Quest } = require("../models");
+const bcrypt = require("bcrypt");
+const Joi = require("joi");
+
+// Schema du mot de passe
+const schema = Joi.object({
+    password: Joi.string()
+      .min(8)
+      .max(30)
+      .pattern(new RegExp('^[a-zA-Z0-9$@$!%*?&_-]+$'))
+      .required()
+  });
+
+
+
 
 const userController = {
     getAllUsers: async (req, res) => {
         try {
-            console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>',User);
             const users = await User.findAll();
             res.status(200).json(users);
         } catch (error) {
@@ -16,8 +29,6 @@ const userController = {
     getOneUser: async (req, res) => {
         try {
             const userId = req.params.id;
-            console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>',userId);
-
             const user = await User.findByPk(userId, {
                 include: [
                     "operations",
@@ -39,45 +50,113 @@ const userController = {
     },
     createUser: async (req, res) => {
         try {
-            const { email, firstname, lastname, password } = req.body;
+            const { email, firstname, lastname, password, confirmation } = req.body;
             const bodyErrors = [];
 
-            if (!email) {
-                bodyErrors.push('email can not be empty');
+            // Test des saisies utilisateurs
+            if (!email || !firstname || !lastname || !password || password != confirmation) {
+                bodyErrors.push('Erreur de saisie du formulaire');
             }
-            if (!firstname) {
-                bodyErrors.push('firstname can not be empty');
+
+            // On vérifie si l'email est déjà prise :
+            const unUtilisateur = await User.findOne({ where: {email:email}});
+            if(unUtilisateur) {
+                bodyErrors.push('Cet email est déjà utilisé')
             }
-            if (!password) {
-                bodyErrors.push('password can not be empty');
+
+            // On test le schéma du mot de passe
+            if(!schema.validate(password)){
+                bodyErrors.push('Mot de passe non conforme');
             }
+
+            // On vérifie si il y a eu des erreurs
             if ( bodyErrors.length) {
                 res.status(400).json(bodyErrors);
+
+
             } else {
-                let newUser = User.build({
+                const encodedPassword = bcrypt.hashSync(password, 5);
+                // ajouter le bcrypt ici
+                let newUser = User.build({      // On crée une instance avec le .build
                     email,
                     firstname,
                     lastname,
-                    password
+                    password:encodedPassword
                 });
+                await newUser.save()            // On enregistre l'instance crée dans la db
+                res.status(200).json(newUser);
             }
 
+        } catch (error) {
+            console.trace(error);
+            res.status(500).json(error.toString());
         }
     },
+    modifyUser: async (req, res) => {
+        try {
+            // On récupère les infos d'un utilisteur : 
+            const userId = req.params.id;
+            const user = await User.findByPk(userId);
+            // On test si cet utilisateur existe, si il n'existe pas on retourne une 404
+            if (!user) {
+                res.status(404).send('Cant find user ' + userId);
+            } else {
+                // on récupère les nouvelles infos dans le body
+                const { firstname, lastname, password, level, wallet, family_id, isAdmin } = req.body;
 
-    // async renderQuizPage(req, res) {
+                // on change que les paramètres présent dans le body
+                if (firstname){
+                    user.firstname = firstname;
+                }
+                if (lastname){
+                    user.lastname = lastname;
+                }
+                if (password){
+                    // On vérifie d'abord le schéma du mot de passe
+                    if(!schema.validate(password)){
+                        res.status(404).send('Invalid password - schema')
+                    }        
+                    // Puis on chiffre le mdp avant de l'inserer en bdd
+                    const encodedPassword = bcrypt.hashSync(password, 5);
+                    user.password = encodedPassword;
+                }
+                if (level){
+                    user.level = level;
+                }
+                if (wallet){
+                    user.wallet = wallet;
+                }
+                if (family_id){
+                    user.family_id = family_id;
+                }
+                if (isAdmin){
+                    user.isAdmin = isAdmin;
+                }
 
-    //     const id = req.params.id
-    
-    //     const quiz = await Quiz.findByPk(id,{
-    //       include: [
-    //         "author",
-    //         "tags",
-    //         {association: "questions", include: ['propositions','level']}
-    //         ]});
-    //     // J'envoie les données à la vue nommée home.ejs
-    //     res.render("quiz",{ quiz });
-    //   },
-}
+
+                // Une fois toutes les modifications faîtes, on enregistre
+                await user.save();
+
+                // Une fois l'instance enregistré, on envoie la liste en réponse dans la req
+                res.status(200).json(user);
+            }   
+        } catch (error) {
+            console.trace(error);
+            res.status(500).json(error.toString());
+        }
+    },
+    deleteUser: async (req, res) => {
+        try {
+            const userId = req.params.id;
+            const user = await user.findByPk(userId); // On instancie la liste a partir de la bdd
+            await user.destroy();  // On utilise destroy() pour supprimer l'enregistrement de la bdd
+            res.status(200).json('OK');
+        } catch(error) {
+            console.trace(error);
+            res.status(500).json(error.toString());
+        }
+    }
+  
+};
 
 module.exports = userController;
--- app/router.js
+++ app/router.js
@@ -9,8 +9,15 @@ const userController = require('./controllers/userController');
 
 const router = express.Router();
 
+/** Users */
 router.get('/users', userController.getAllUsers);
 router.get('/user/:id', userController.getOneUser);
+router.post('/users', userController.createUser);
+router.put('/user/:id', userController.modifyUser);
+router.delete('/user/:id', userController.deleteUser);
+
+
+
 
 
 module.exports = router;
