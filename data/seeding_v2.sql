3b6547875a0e6a30382d9288a5f8a89c99aaefd5
EchterhoffAlexandre <echterhoff.a@gmail.com>
2023-04-21 09:33:53 +0200

    merge de backoffice & artistepeintre

--- data/seeding_v2.sql
+++ data/seeding_v2.sql
@@ -11,7 +11,7 @@ VALUES ('echterhoff.a@gmail.com', 'echterhoff', 'alexandre', 'azerty', true, 1),
 ('guillaumegandolfi@gmail.com','guillaume', 'gandolfi', 'abcd', true, 1),
 ('abdelaziz@gmail.com', 'abdel', 'aziz', 'azerty', false, 2),
 ('peterMounier@gmail.com', 'peter', 'mounier', 'azerty', false, 2),
-('sondes@gmail.com', 'sondes', 'nefzi', 'azerty', false, 2);
+('sondes@gmail.com', 'sondes', 'nefzi', 'azerty', false, 2),
 ('john@johndoe.com', 'john', 'doe', '$2b$05$i6Hb8iqIYUPNDEeKZ5VX5uEeLkmDwKcdUYFlu/60jvX4pKeGVB/b.', false, 1),
 ('john@johndoee.com', 'john', 'doee', '$2b$05$qGANHBWteT9Ffwdr56RbzuDHIBihNhUGBAxMi6nkBAL5OjJ07y5k2', true, 1);
 -- Les deux johndoe mdp = azerty
