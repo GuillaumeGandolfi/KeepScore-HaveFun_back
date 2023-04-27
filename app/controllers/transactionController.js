8f430f9e60ff84369dc1379315ffaf36d1c8e8c8
EchterhoffAlexandre <echterhoff.a@gmail.com>
2023-04-27 09:39:17 +0200

    update seeding + method transaction monthly

--- app/controllers/transactionController.js
+++ app/controllers/transactionController.js
@@ -11,6 +11,9 @@ const {
     Op
 } = require('sequelize');
 const dayjs = require('dayjs');
+const jwt = require('jsonwebtoken');
+
+
 
 const transactionController = {
     getAllTransactions: async (req, res) => {
@@ -225,6 +228,7 @@ const transactionController = {
     },
     getTransactionOfMonth: async (req, res) => {
         try {
+            
             // Je récupère l'userId grâce au token d'authentification
             const token = req.headers.authorization.split(' ')[1];
             const decodedToken = jwt.verify(token, 'secret-key');
@@ -233,7 +237,7 @@ const transactionController = {
             const now = new Date();
 
             // Trouver la date du premier jour du mois courant
-            const startOfMonth = dayjs(now).startOfMonth('month').toDate();
+            const startOfMonth = dayjs(now).startOf('month').toDate();
 
             // Trouver la date du dernier jour du mois courant
             const endOfMonth = dayjs(now).endOf('month').toDate();
