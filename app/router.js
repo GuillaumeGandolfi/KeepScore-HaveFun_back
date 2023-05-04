b5c02ab0a6de8ef27df2480403e2b552c4218d72
GuillaumeGandolfi <gandolfi.guillaume@gmail.com>
2023-05-04 15:55:17 +0200

    fix

--- app/router.js
+++ app/router.js
@@ -21,56 +21,56 @@ const tokenMiddleware = require('./middlewares/tokenMiddleware');
 const router = express.Router();
 
 /** Users */
-router.get('/users',  userController.getAllUsers);
+router.get('/users', userController.getAllUsers);
 router.get('/user', tokenMiddleware, userController.getOneUser);
 router.get('/user-quest', tokenMiddleware, userController.getAllQuestOfOneUser);
-router.post('/user',  userController.createUser);
+router.post('/user', userController.createUser);
 router.put('/user', tokenMiddleware, userController.modifyUser);
-router.delete('/user', tokenMiddleware,  userController.deleteUser);
-router.post('/user/add-friend', tokenMiddleware,  userController.addFriend);
+router.delete('/user', tokenMiddleware, userController.deleteUser);
+router.post('/user/add-friend', tokenMiddleware, userController.addFriend);
 router.post('/user/finish-quest', tokenMiddleware, userController.userFinishQuest);
 router.delete('user/transaction-budget-delete', tokenMiddleware, userController.deleteAllTransactionBudgetFromOneUser);
 
 /** Families */
-router.get('/families',  familyController.getAllFamilies);
-router.get('/family/:id',  familyController.getOneFamily);
-router.post('/family',  familyController.createFamily);
-router.put('/family/:id',  familyController.modifyFamily);
-router.delete('/family/:id',  familyController.deleteFamily);
+router.get('/families', familyController.getAllFamilies);
+router.get('/family/:id', familyController.getOneFamily);
+router.post('/family', familyController.createFamily);
+router.put('/family/:id', familyController.modifyFamily);
+router.delete('/family/:id', familyController.deleteFamily);
 
 /** Quests */
-router.get('/quests',  questController.getAllQuests);
-router.get('/quest/:id',  questController.getOneQuest);
-router.post('/quest',  questController.createQuest);
-router.put('/quest/:id',  questController.modifyQuest);
-router.delete('/quest/:id',  questController.deleteQuest);
+router.get('/quests', questController.getAllQuests);
+router.get('/quest/:id', questController.getOneQuest);
+router.post('/quest', questController.createQuest);
+router.put('/quest/:id', questController.modifyQuest);
+router.delete('/quest/:id', questController.deleteQuest);
 
 
 /** Collections */
-router.get('/collections',  collectionController.getAllCollections);
-router.get('/collection/:id',  collectionController.getOneCollection);
+router.get('/collections', collectionController.getAllCollections);
+router.get('/collection/:id', collectionController.getOneCollection);
 router.post('/collection', collectionController.createCollection);
 router.put('/collection/:id', collectionController.modifyCollection);
 router.delete('/collection/:id', collectionController.deleteCollection);
 
 /** Shops */
-router.get('/shops',  shopController.getAllItemShop);
+router.get('/shops', shopController.getAllItemShop);
 router.get('/shop/:id', shopController.getOneItem);
 router.post('/shop/:id', shopController.addItemToShop);
-router.delete('/shop/:id',  shopController.deleteItemFromShop);
+router.delete('/shop/:id', shopController.deleteItemFromShop);
 
 /** Transactions */
-router.get('/transactions',  transactionController.getAllTransactions);
+router.get('/transactions', transactionController.getAllTransactions);
 router.get('/transactions/budget/:id', transactionController.getAllTransactionsOfBudget);
-router.get('/transaction/:id',  transactionController.getOneTransaction);
+router.get('/transaction/:id', transactionController.getOneTransaction);
 router.get('/transactions/user/:id', transactionController.getAllTransactionsOfUser);
-router.get('/transaction/day/:id',tokenMiddleware, transactionController.getTransactionOfToday);
-router.get('/transaction/week/:id',tokenMiddleware, transactionController.getTransactionOfWeek);
-router.get('/transaction/month/:id',tokenMiddleware, transactionController.getTransactionOfMonth);
-router.get('/transaction/year/:id',tokenMiddleware, transactionController.getTransactionOfYear);
+router.get('/transaction/day/:id', tokenMiddleware, transactionController.getTransactionOfToday);
+router.get('/transaction/week/:id', tokenMiddleware, transactionController.getTransactionOfWeek);
+router.get('/transaction/month/:id', tokenMiddleware, transactionController.getTransactionOfMonth);
+router.get('/transaction/year/:id', tokenMiddleware, transactionController.getTransactionOfYear);
 router.post('/transaction', tokenMiddleware, transactionController.createTransaction);
 router.put('/transaction/:id', transactionController.modifyTransaction);
-router.delete('/transaction/:id',  transactionController.deleteTransaction);
+router.delete('/transaction/:id', transactionController.deleteTransaction);
 
 
 /** Budget */
